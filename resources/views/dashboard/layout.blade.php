e72b3163abd4f0f9112d957d7d410dc35d96efbe
Fauzi-Taufiq <muhtaufiq684@gmail.com>
2025-07-09 20:08:10 +0700

    add filter, cdn

--- resources/views/dashboard/layout.blade.php
+++ resources/views/dashboard/layout.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Dashboard') - Nexakun</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src="{{ asset('js/dashboard.js') }}"></script>
     <script>
         tailwind.config = {
             darkMode: 'class',
